3d7b38d68df753b0beb6c4e5a04f0e50005bd0af
JulSarmiento <jasa1999@hotmail.com>
2022-08-23 15:23:12 -0500

    ya funciona el buscador, falta que se haga el fecth y pinte al bichito

--- src/Components/SearchForm/Index.jsx
+++ src/Components/SearchForm/Index.jsx
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from "react"
+import React, {useState } from "react"
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 import './index.css'
 
-const URL = "https://pokeapi.co/api/v2/pokemon"
+const URL = "https://pokeapi.co/api/v2/pokemon/"
 
-export default function SearchForm() {
+export default function SearchForm({name}) {
 
   const [pokemon, setPokemon] = useState();
 
   const handlerOnChange = (e) => {
-    setPokemon({...pokemon, [e.target.name] : e.target.value})
+    setPokemon(e.target.value)
   }
 
   const fetchPokemon = async (name) => {
-    const res = await fetch(`${URL}/${pokemon.name}`);
+    const res = await fetch(`${URL}${name}/`);
     const json = await res.json();
     return json
   };
 
+  console.log(pokemon)
 
   const submit = (e) => {      
     e.preventDefault();
-    fetchPokemon(pokemon.name)
+    fetchPokemon(pokemon)
    }
 
    return (
     <Form className="form-container" onSubmit={submit}>
+
       <Form.Group className="mb-3"  controlId="formBasicPassword">
         <Form.Label></Form.Label>
-        <Form.Control type="text" onChange={handlerOnChange} placeholder="Nombre del Pokemon" />
+        <Form.Control type="text" name="pokemon-to-search" onChange={handlerOnChange} placeholder="Nombre del Pokemon" />
       </Form.Group>
       <Button variant="primary" className="btn-form" type="submit">
         Buscar
